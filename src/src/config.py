19d6697c39487b88b6cc9687cf60fec64ee0e337
diex360 <sandovaldiegoalejandro15@gmail.com>
2026-09-22 11:29:06 -0500

    Delete src/src directory

--- src/src/config.py
+++
@@ -1,5 +0,0 @@
-"""Constantes compartidas por TODOS los roles. Nadie las redefine en su notebook."""
-SEMILLA = 42
-R_TIERRA_KM = 6371.0
-LAT_C, LON_C = 40.7510, -73.9737     # centro de proyección (media de recogidas en la muestra)
-LADO_ZONA_KM = 1.0                   # D-06
